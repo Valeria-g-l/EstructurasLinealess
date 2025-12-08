1620f08b48a7af35d3fd70ea1a6e86c64a5377a5
Valeria <valeriaguamanleon03@gmail.com>
2025-12-08 12:48:15 -0500

    Estructuras Lineales

---
+++ src/App.java
@@ -0,0 +1,17 @@
+import models.Node;
+import models.Person;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Node<Person> node1 = new Node<Person>(new Person("Juan", 30));
+        Node<Person> node2 = new Node<Person>(new Person("Maria", 35));
+        Node<Person> node3 = new Node<Person>(new Person("Pedro", 25));
+        Node<Person> node4 = new Node<Person>(new Person("Diego", 28));
+        node1.setNext(node2);
+        node2.setNext(node3); 
+        node3.setNext(node4);
+
+        System.out.println(node1);
+        
+    }
+}
